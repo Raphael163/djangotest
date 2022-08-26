133039446ae48f4862f976fdc045e155e0685df3
Raphael163 <orbita.route@yandex.ru>
2022-08-26 21:47:50 +0400

    one push

---
+++ news/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import News
+
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("id", 'title', "created_at", 'update_at', "is_published")
+
+
+admin.site.register(News, NewsAdmin)
