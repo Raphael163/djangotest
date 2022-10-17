adc6ce0573b283852db3177f14c13aeedae96749
Raphael163 <orbita.route@yandex.ru>
2022-10-17 17:01:16 +0400

    first commit

--- news/admin.py
+++ news/admin.py
@@ -2,10 +2,23 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import News
+from .models import Category
 
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = ("id", 'title', "created_at", 'update_at', "is_published")
+    search_fields = ("title", "content")
+    list_display_links = ("id", "title")
+    list_editable = ("is_published",)
+    list_filter = ("is_published",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", 'title', "news")
+    list_display_links = ("id", "title")
+    search_fields = ("title",)
+    list_filter = ("id", "title")
 
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
